91d99393d8b9da33ea63120b622ac3e9bed402b2
Sammy Samkough <sammysamkough@gmail.com>
2018-11-14 09:46:13 -0500

    Created grab_sample_size()

--- code/classifiers/random-forest-01.py
+++ code/classifiers/random-forest-01.py
@@ -5,6 +5,25 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import random
 
+# grabbing the sample size
+def grab_sample_size (input_path, features):
+  yesorno = input("\nWould you like to grab a sample of the data from " + input_path + "?\n")
+  if yesorno == "yes":
+    n = sum(1 for line in open(input_path)) - 1 # number of records in file (excludes header)
+    s = input("\nWhat is your desired sample size?\n") # desired sample size
+    s = int(s)
+    skip = sorted(random.sample(range(1, n+1), n-s)) # the 0-indexed header will not be included in the skip list
+    df = pd.read_csv(input_path, header = None, names = features, skiprows = skip)
+  elif yesorno == "1000":
+    n = sum(1 for line in open(input_path)) - 1
+    s = 1000
+    skip = sorted(random.sample(range(1, n+1), n-s))
+    df = pd.read_csv(input_path, header = None, names = features, skiprows = skip)
+  else:
+    df = pd.read_csv(input_path, header = None, names = features)
+
+  return df
+
 # convert categorical features to numerical vectors
 # generate 5-class labels
 def preprocess_five_class (input_path):
@@ -22,21 +41,7 @@ def preprocess_five_class (input_path):
   r2l_attacks = ['warezmaster', 'warezclient', 'ftp_write', 'guess_passwd', 'imap', 'multihop', 'phf', 'spy', 'sendmail', 'named', 'snmpgetattack', 'snmpguess', 'xlock', 'xsnoop', 'worm']
   u2r_attacks = ['rootkit', 'buffer_overflow', 'loadmodule', 'perl', 'httptunnel', 'ps', 'sqlattack', 'xterm']
 
-  # grab a sample size
-  yesorno = input("\nWould you like to grab a sample of the data from " + input_path + "?\n")
-  if yesorno == "yes":
-    n = sum(1 for line in open(input_path)) - 1 # number of records in file (excludes header)
-    s = input("\nWhat is your desired sample size?\n") # desired sample size
-    s = int(s)
-    skip = sorted(random.sample(range(1, n+1), n-s)) # the 0-indexed header will not be included in the skip list
-    df = pd.read_csv(input_path, header = None, names = features, skiprows = skip)
-  elif yesorno == "1000":
-    n = sum(1 for line in open(input_path)) - 1
-    s = 1000
-    skip = sorted(random.sample(range(1, n+1), n-s))
-    df = pd.read_csv(input_path, header = None, names = features, skiprows = skip)
-  else:
-    df = pd.read_csv(input_path, header = None, names = features)
+  df = grab_sample_size(input_path, features)
     
   for x in dos_attacks:
       df['label'].replace(x+'.', 'dos', inplace=True)
