bbf30afebe3fc611a03946d6e09373cb55787730
Sammy Samkough <sammysamkough@gmail.com>
2018-11-29 15:48:14 -0500

    Added 2-class functionality

--- code/classifiers/random-forest.py
+++ code/classifiers/random-forest.py
@@ -27,7 +27,7 @@ def grab_sample_size (input_path, features):
   return df
 
 # convert categorical features to numerical vectors
-# generate 5-class labels
+# generate 2-class or 5-class labels
 def preprocess_five_class (input_path):
   features = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land',
           'wrong_fragment', 'urgent', 'hot', 'num_failed_logins', 'logged_in', 'num_compromised',
@@ -38,6 +38,11 @@ def preprocess_five_class (input_path):
           'dst_host_same_srv_rate', 'dst_host_diff_srv_rate', 'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate',
           'dst_host_serror_rate', 'dst_host_srv_serror_rate', 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate', 'label']
 
+  attacks = ['back', 'neptune', 'smurf', 'teardrop', 'land', 'pod', 'apache2', 'mailbomb', 'processtable', 'udpstorm',
+             'satan', 'portsweep', 'ipsweep', 'nmap', 'mscan', 'saint', 'warezmaster', 'warezclient', 'ftp_write',
+             'guess_passwd', 'imap', 'multihop', 'phf', 'spy', 'sendmail', 'named', 'snmpgetattack', 'snmpguess', 'xlock',
+             'xsnoop', 'worm', 'rootkit', 'buffer_overflow', 'loadmodule', 'perl', 'httptunnel', 'ps', 'sqlattack', 'xterm']
+  
   dos_attacks = ['back', 'neptune', 'smurf', 'teardrop', 'land', 'pod', 'apache2', 'mailbomb', 'processtable', 'udpstorm']
   prob_attacks = ['satan', 'portsweep', 'ipsweep', 'nmap', 'mscan', 'saint']
   r2l_attacks = ['warezmaster', 'warezclient', 'ftp_write', 'guess_passwd', 'imap', 'multihop', 'phf', 'spy', 'sendmail', 'named', 'snmpgetattack', 'snmpguess', 'xlock', 'xsnoop', 'worm']
@@ -45,14 +50,25 @@ def preprocess_five_class (input_path):
 
   df = grab_sample_size(input_path, features)
     
-  for x in dos_attacks:
-      df['label'].replace(x+'.', 'dos', inplace=True)
-  for x in prob_attacks:
-      df['label'].replace(x+'.', 'prob', inplace=True)
-  for x in r2l_attacks:
-      df['label'].replace(x+'.', 'r2l', inplace=True)
-  for x in u2r_attacks:
-      df['label'].replace(x+'.', 'u2r', inplace=True)
+  running = True
+  while (running):
+    yesorno = input("\nWould you like to do 2-classification or 5-classification?\n")
+    if yesorno == "2":
+      for x in attacks:
+          df['label'].replace(x+'.', 'attack', inplace=True)
+      running = False
+    elif yesorno == "5":      
+      for x in dos_attacks:
+          df['label'].replace(x+'.', 'dos', inplace=True)
+      for x in prob_attacks:
+          df['label'].replace(x+'.', 'prob', inplace=True)
+      for x in r2l_attacks:
+          df['label'].replace(x+'.', 'r2l', inplace=True)
+      for x in u2r_attacks:
+          df['label'].replace(x+'.', 'u2r', inplace=True)
+      running = False
+    else:
+      print("Please either input '2' for 2-classification or '5' for 5-classification..")
       
   categorical_columns = ['protocol_type', 'service', 'flag', 'label']
   df[categorical_columns] = df[categorical_columns].astype('category').apply(lambda x: x.cat.codes)
